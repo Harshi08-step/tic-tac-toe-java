ad66bce8e5fec63d4f51bfc0d0607a70f1ed5b84
Harshi08-step <harshi0807@gmail.com>
2026-05-01 15:21:50 +0530

    Implement UC9: win detection logic (rows, columns, diagonals)

--- tictactoe.java
+++ tictactoe.java
@@ -1,62 +1,27 @@
-public class TicTacToe {
+public boolean checkWin() {
 
-    private char[] board = new char[9];
-    private char currentPlayer = 'X';
-
-    public TicTacToe() {
-        for (int i = 0; i < 9; i++) {
-            board[i] = (char) ('1' + i);
+    // Rows
+    for (int i = 0; i < 9; i += 3) {
+        if (board[i] == board[i + 1] && board[i] == board[i + 2]) {
+            return true;
         }
     }
 
-    // Switch turns
-    public void switchTurn() {
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    // Columns
+    for (int i = 0; i < 3; i++) {
+        if (board[i] == board[i + 3] && board[i] == board[i + 6]) {
+            return true;
+        }
     }
 
-    // Check draw condition
-    public boolean isBoardFull() {
-        for (char c : board) {
-            if (c != 'X' && c != 'O') {
-                return false;
-            }
-        }
+    // Diagonals
+    if (board[0] == board[4] && board[0] == board[8]) {
         return true;
     }
 
-    // Placeholder for win check (UC9 will implement real logic)
-    public boolean checkWin() {
-        return false;
+    if (board[2] == board[4] && board[2] == board[6]) {
+        return true;
     }
 
-    // Game loop (UC8)
-    public void startGame() {
-
-        boolean gameRunning = true;
-
-        while (gameRunning) {
-
-            System.out.println("Player turn: " + currentPlayer);
-
-            // UC3 / UC6 / UC7 will plug in here later
-            // Example: updateBoard(slot, currentPlayer);
-
-            if (checkWin()) {
-                System.out.println(currentPlayer + " wins!");
-                break;
-            }
-
-            if (isBoardFull()) {
-                System.out.println("Game Draw!");
-                break;
-            }
-
-            switchTurn();
-        }
-    }
-
-    public static void main(String[] args) {
-        TicTacToe game = new TicTacToe();
-        game.startGame();
-    }
+    return false;
 }
